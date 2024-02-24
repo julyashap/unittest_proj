872d2d00f608bc3dd3ce31005fdf94778b146611
Julya Shapaeva <shapaeva03@mail.ru>
2024-02-25 02:15:04 +0300

    Add tests for my_slice()

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -11,3 +11,9 @@ class TestArrs(unittest.TestCase):
     def test_slice(self):
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
         self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+
+    def test_slice_empty_list(self):
+        self.assertEqual(arrs.my_slice([], 0, 0), [])
+
+    def test_slice_no_start(self):
+        self.assertEqual(arrs.my_slice([1, 2, 3], end=1), [1])
